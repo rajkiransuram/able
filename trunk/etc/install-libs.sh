6bda035c3635e5dba74155b72442dbaf188e6f0f
plightbo <plightbo@ebce899e-a133-11dd-9a14-c1645b63b0c6>
2008-11-05 19:15:39 +0000

    upgraded to latest stripes and hibernate releases, which have the necessary bits needed to make Able really rock

--- trunk/etc/install-libs.sh
+++ trunk/etc/install-libs.sh
@@ -1,6 +1,13 @@
 mvn install:install-file -DgroupId=warp-persist -DartifactId=warp-persist -Dversion=1.0.1 -Dpackaging=jar -Dfile=warp-persist-1.0.1.jar
-mvn install:install-file -DgroupId=javax.transaction -DartifactId=jta -Dversion=1.0.1B -Dpackaging=jar -Dfile=jta-1.0.1B.jar
+
 mvn install:install-file -DgroupId=com.google.code.guice -DartifactId=guice-servlet -Dversion=1.0 -Dpackaging=jar -Dfile=guice-servlet-1.0.jar
-mvn install:install-file -DgroupId=org.stripesframework -DartifactId=stripes -Dversion=1.5-SNAPSHOT-950 -Dpackaging=jar -Dfile=stripes-1.5-SNAPSHOT-950.jar
-mvn install:install-file -DgroupId=org.stripesframework -DartifactId=stripes -Dversion=1.5-SNAPSHOT-950 -Dclassifier=sources -Dpackaging=jar -Dfile=stripes-1.5-SNAPSHOT-950-src.jar
+
+mvn install:install-file -DgroupId=org.stripesframework -DartifactId=stripes -Dversion=1.5.1-SNAPSHOT -Dpackaging=jar -Dfile=stripes-1.5.1-SNAPSHOT.jar
+mvn install:install-file -DgroupId=org.stripesframework -DartifactId=stripes -Dversion=1.5.1-SNAPSHOT -Dclassifier=sources -Dpackaging=jar -Dfile=stripes-1.5.1-SNAPSHOT-src.jar
+
 mvn install:install-file -DgroupId=org.directwebremoting -DartifactId=dwr -Dversion=3.0.0.109.dev -Dpackaging=jar -Dfile=dwr-3.0.0.109.dev.jar
+
+mvn install:install-file -DgroupId=org.hibernate -DartifactId=hibernate-parent -Dversion=3.3.2-SNAPSHOT -Dpackaging=pom -Dfile=hibernate-parent-3.3.2-SNAPSHOT.pom
+mvn install:install-file -DgroupId=org.hibernate -DartifactId=hibernate-core -Dversion=3.3.2-SNAPSHOT -Dpackaging=jar -Dfile=hibernate-core-3.3.2-SNAPSHOT.jar
+mvn install:install-file -DgroupId=org.hibernate -DartifactId=hibernate-core -Dversion=3.3.2-SNAPSHOT -Dpackaging=pom -Dfile=hibernate-core-3.3.2-SNAPSHOT.pom
+mvn install:install-file -DgroupId=org.hibernate -DartifactId=hibernate-validator -Dversion=3.1.0.GA -Dpackaging=jar -Dfile=hibernate-validator-3.1.0.GA.jar
