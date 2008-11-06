87b2f6d0fd938d1d849fd0fbeda0d0b3c3b22be0
plightbo <plightbo@ebce899e-a133-11dd-9a14-c1645b63b0c6>
2008-11-06 13:39:05 +0000

    poof

--- src/main/java/able/stripes/util/JSP.java
+++
@@ -1,9 +0,0 @@
-package able.stripes.util;
-
-import net.sourceforge.stripes.action.ForwardResolution;
-
-public class JSP extends ForwardResolution {
-    public JSP(String path) {
-        super("/WEB-INF/jsp/" + path);
-    }
-}
